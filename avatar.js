0236c637a074f542b5af0996c2870d83fee1d21b
Josh <61258880+guccifrog@users.noreply.github.com>
2020-02-19 21:41:03 -0500

    Add files via upload

---
+++ avatar.js
@@ -0,0 +1,19 @@
+﻿const Discord = require("discord.js")
+const botconfig = require("../../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+	let user = message.mentions.users.first() || message.author;
+	
+	    let avatarembed = new Discord.RichEmbed()
+      .setColor(botconfig["bot_setup"].main_embed_color)
+      .setAuthor(`${user.username}'s Avatar`)
+      .setImage(user.avatarURL)
+    message.channel.send(avatarembed).then(msg => msg.delete(10000));
+		
+}
+
+module.exports.help = {
+    name: "avatar",
+    name2: "pfp",
+}
