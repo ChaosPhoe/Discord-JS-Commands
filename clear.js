718785b4202fb4e16b85e59a077efaeef23ce8dd
Josh <61258880+guccifrog@users.noreply.github.com>
2020-03-09 15:14:29 -0400

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	message.delete();
+	
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Invalid permissions.").then(msg => msg.delete(10000));
+    if(!args[0]) return message.channel.send("Please provide the amount of messages to bulk delete.").then(msg => msg.delete(5000));
+	if(args[0] > 100) return message.channel.send("Please provide an amount of messages lower then 100 to delete.").then(msg => msg.delete(5000));
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`${args[0]} messages have been Bulk Deleted in <#${message.channel.id}>.`).then(msg => msg.delete(10000));
+    });
+}
+
+module.exports.help = {
+    name: "clear",
+    name2: "purge"
+}
